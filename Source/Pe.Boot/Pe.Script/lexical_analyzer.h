4a5f46868006980b65ada89f3d680b149a923e53
sk <content.type.text.net@gmail.com>
2021-10-25 20:59:54 +0900

    解析処理入門中

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.h
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.h
@@ -15,13 +15,48 @@ typedef struct tag_SOURCE_TOKEN
     /// 文字の開始位置(0始まり)。
     /// </summary>
     size_t column_position;
+    /// <summary>
+    /// トークンデータ。
+    /// </summary>
+    TEXT value;
 } SOURCE_TOKEN;
 
 typedef SOURCE_TOKEN* SOURCE_TOKENS;
 
-typedef struct tag_SOURCE_FILE
+/// <summary>
+/// ソースデータ情報。
+/// </summary>
+typedef struct tag_SOURCE_DATA
 {
+    /// <summary>
+    /// ソースファイルパス。
+    /// 解放が必要(free_source_dataで巻き取る)。
+    /// </summary>
     TEXT path;
+    /// <summary>
+    /// ソース読み込み時のトークン一覧。
+    /// </summary>
     SOURCE_TOKENS tokens;
+    /// <summary>
+    /// ソース読み込み時のトークン数。
+    /// 解放が必要(free_source_dataで巻き取る)。
+    /// </summary>
     size_t token_count;
-} SOURCE_FILE;
+} SOURCE_DATA;
+
+typedef struct tag_PROJECT_SETTING
+{
+    TEXT base;
+} PROJECT_SETTING;
+
+
+/// <summary>
+///
+/// </summary>
+/// <param name="path"></param>
+/// <param name="source"></param>
+/// <returns><see cref="free_source_data(SOURCE_DATA*)" />が必要。</returns>
+SOURCE_DATA parse_source_data(const TEXT* path, const TEXT* source);
+
+bool free_source_data(SOURCE_DATA* source_data);
+
