738b61f37c45d06983f00f72f89933ea2817547a
sk <content.type.text.net@gmail.com>
2021-10-28 20:14:40 +0900

    ふわふわ実装

---
+++ Source/Pe.Boot/Pe.Script/script.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../Pe.Library/text.h"
+
+/// <summary>
+/// プロジェクト情報。
+/// 解析時に使用される。
+/// </summary>
+typedef struct tag_PROJECT_INFO
+{
+    /// <summary>
+    /// スクリプトのベースディレクトリパス。
+    /// </summary>
+    TEXT base_directory_path;
+    /// <summary>
+    /// 警告はエラーとして扱うか。
+    /// </summary>
+    bool warning_is_error;
+} PROJECT_INFO;
+
+
+
