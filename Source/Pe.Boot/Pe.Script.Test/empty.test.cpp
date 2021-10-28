aeb52757604b9f3015607ac32deb41411f5696a0
sk <content.type.text.net@gmail.com>
2021-10-28 15:50:06 +0900

    無理やり感がすごいけどたぶんこれで動くんじゃないですかね！

---
+++ Source/Pe.Boot/Pe.Script.Test/empty.test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+
+extern "C" {
+#   include "../Pe.Library/text.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ScriptTest
+{
+    TEST_CLASS(empty_test)
+    {
+    public:
+
+        TEST_METHOD(test)
+        {
+            TEXT expected = wrap("C:\\abc\\bin\\Pe.Main.exe");
+            TEXT actual = wrap("C:\\abc\\bin\\Pe.Main.exe");
+
+            Assert::AreEqual(expected.value, actual.value);
+        }
+    };
+}
