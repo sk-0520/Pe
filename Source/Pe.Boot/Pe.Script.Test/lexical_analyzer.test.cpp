ad814ce4bfaa13f1ff850744ab4b6a8d7eb4c94c
sk <content.type.text.net@gmail.com>
2021-10-30 15:11:47 +0900

    つらつら

---
+++ Source/Pe.Boot/Pe.Script.Test/lexical_analyzer.test.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+
+extern "C" {
+#   include "../Pe.Script/lexical_analyzer.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ScriptTest
+{
+    TEST_CLASS(lexical_analyzer_test)
+    {
+    public:
+
+        TEST_METHOD(analyze_empty_test)
+        {
+            PROJECT_SETTING setting;
+            TOKEN_RESULT token_result = analyze(NULL, &setting);
+
+            Assert::AreEqual((size_t)0, token_result.token.length);
+            Assert::AreEqual((size_t)0, token_result.result.length);
+            free_token_result(&token_result);
+        }
+    };
+}
