5240c778b4e43dc345e3b9e44c201557e7093ab2
sk <content.type.text.net@gmail.com>
2021-10-30 15:11:47 +0900

    つらつら

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.c
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.c
@@ -26,3 +26,54 @@ bool free_token_pairs(TOKEN_PAIR pairs[], size_t length)
 
     return true;
 }
+
+typedef struct tag_ANALYZE_DATA
+{
+    TOKEN_RESULT* result;
+    PROJECT_SETTING* setting;
+} ANALYZE_DATA;
+
+void analyze_line(TOKEN_RESULT* result, size_t line_number, const TEXT* line, const PROJECT_SETTING* setting)
+{
+
+}
+
+bool foreach_analyze_line(const void* value, size_t index, size_t length, void* data)
+{
+    TEXT* line = (TEXT*)value;
+    ANALYZE_DATA* analyze_data = (ANALYZE_DATA*)data;
+    analyze_line(analyze_data->result, index + 1, line, analyze_data->setting);
+    return true;
+}
+
+TOKEN_RESULT analyze(const TEXT* source, const PROJECT_SETTING* setting)
+{
+    assert(setting);
+
+    TOKEN_RESULT token_result = {
+        .token = create_object_list(sizeof(TOKEN), 1024, compare_object_list_value_null, free_object_list_value_null),
+        .result = create_object_list(sizeof(COMPILE_RESULT), OBJECT_LIST_DEFAULT_CAPACITY_COUNT, compare_object_list_value_null, free_object_list_value_null),
+    };
+
+    if (!source || !source->length) {
+        return token_result;
+    }
+
+    OBJECT_LIST lines = split_newline_text(source);
+
+    ANALYZE_DATA data = {
+        .result = &token_result,
+        .setting = (PROJECT_SETTING*)setting, //TODO: とりあえずの回避。Cって構造体メンバにconst使えんの？
+    };
+    foreach_object_list(&lines, foreach_analyze_line, &data);
+
+    free_object_list(&lines);
+
+    return token_result;
+}
+
+void free_token_result(TOKEN_RESULT* token_result)
+{
+    free_object_list(&token_result->token);
+    free_object_list(&token_result->result);
+}
