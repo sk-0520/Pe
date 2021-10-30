0c76bb10262a1b1c728a88782a4f07beec36c978
sk <content.type.text.net@gmail.com>
2021-10-30 15:11:47 +0900

    つらつら

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.h
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.h
@@ -5,6 +5,17 @@
 
 #include "token.h"
 
+typedef struct tag_TOKEN_RESULT
+{
+    OBJECT_LIST token;
+    OBJECT_LIST result;
+} TOKEN_RESULT;
+
+
 size_t allocate_token_pairs(TOKEN_PAIR** pairs);
 
 bool free_token_pairs(TOKEN_PAIR pairs[], size_t length);
+
+TOKEN_RESULT analyze(const TEXT* source, const PROJECT_SETTING* setting);
+
+void free_token_result(TOKEN_RESULT* token_result);
