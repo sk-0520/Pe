ffc46b6849771cf3299812cca2d6d9497f34ad10
sk <content.type.text.net@gmail.com>
2021-10-30 20:51:35 +0900

    analyze_block_comment_test

---
+++ Source/Pe.Boot/Pe.Script/compile.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+/// <summary>
+/// コンパイル段階。
+/// </summary>
+typedef enum tag_COMPILE_STAGE
+{
+    COMPILE_STAGE_NONE,
+    /// <summary>
+    /// 字句解析。
+    /// </summary>
+    COMPILE_STAGE_LEX,
+} COMPILE_STAGE;
+
+/// <summary>
+/// エラーコード。
+/// </summary>
+typedef enum tag_COMPILE_CODE
+{
+    COMPILE_CODE_NONE,
+    COMPILE_CODE_NOT_CLOSE_COMMENT,
+} COMPILE_CODE;
