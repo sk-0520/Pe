65c273ccefdc2b5030ee6f8f54d73bc1f256666a
sk <content.type.text.net@gmail.com>
2021-10-30 18:48:36 +0900

    複数行処理に切り替えようと思ってる。だけ

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.h
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.h
@@ -7,14 +7,11 @@
 
 typedef struct tag_TOKEN_RESULT
 {
+    TEXT* file_path;
     OBJECT_LIST token;
     OBJECT_LIST result;
 } TOKEN_RESULT;
 
-
-size_t allocate_token_pairs(TOKEN_PAIR** pairs);
-
-bool free_token_pairs(TOKEN_PAIR pairs[], size_t length);
 
 TOKEN_RESULT analyze(const TEXT* file_path, const TEXT* source, const PROJECT_SETTING* setting);
 
