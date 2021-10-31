43cd3ce487b2e0f9333cee87c1b064b06ebbfa7f
sk <content.type.text.net@gmail.com>
2021-10-31 21:19:31 +0900

    細かく分けたいけど分けすぎるとなぁ

---
+++ Source/Pe.Boot/Pe.Script/source.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <stddef.h>
+
+/// <summary>
+/// ソース位置。
+/// </summary>
+typedef struct tag_SOURCE_POSITION
+{
+    /// <summary>
+    /// 行番号(1始まり)。
+    /// </summary>
+    size_t line_number;
+    /// <summary>
+    /// カラム位置(0始まり)。
+    /// </summary>
+    size_t column_position;
+} SOURCE_POSITION;
+
