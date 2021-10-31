0fc82bbd07380dac9c21f37d20513de238bcaf03
sk <content.type.text.net@gmail.com>
2021-10-31 21:19:31 +0900

    細かく分けたいけど分けすぎるとなぁ

--- Source/Pe.Boot/Pe.Script/compile.h
+++ Source/Pe.Boot/Pe.Script/compile.h
@@ -1,4 +1,10 @@
 ﻿#pragma once
+#include <stddef.h>
+
+#include "../Pe.Library/text.h"
+#include "../Pe.Library/object_list.h"
+
+#include "source.h"
 
 /// <summary>
 /// コンパイル段階。
@@ -26,3 +32,39 @@ typedef enum tag_COMPILE_CODE
     COMPILE_CODE_INVALID_NUMBER,
     COMPILE_CODE_PAESE_ERROR_NUMBER,
 } COMPILE_CODE;
+
+/// <summary>
+/// コンパイル結果種別。
+/// </summary>
+typedef enum tag_COMPILE_RESULT_KIND
+{
+    COMPILE_RESULT_KIND_INFORMATION,
+    COMPILE_RESULT_KIND_WARNING,
+    COMPILE_RESULT_KIND_ERROR,
+} COMPILE_RESULT_KIND;
+
+/// <summary>
+/// コンパイル結果。
+/// </summary>
+typedef struct tag_COMPILE_RESULT
+{
+    COMPILE_STAGE stage;
+    COMPILE_RESULT_KIND kind;
+    SOURCE_POSITION position;
+    COMPILE_CODE code;
+    /// <summary>
+    /// 解放が必要。
+    /// </summary>
+    TEXT remark;
+} COMPILE_RESULT;
+
+/// <summary>
+///
+/// </summary>
+/// <param name="compile_results"><c>COMPILE_RESULT</c>の<c>OBJECT_LIST</c></param>
+/// <param name="kind"></param>
+/// <param name="code"></param>
+/// <param name="remark"></param>
+/// <param name="column_position"></param>
+/// <param name="line_number"></param>
+void add_compile_result(OBJECT_LIST* compile_results, COMPILE_RESULT_KIND kind, COMPILE_CODE code, const TEXT* remark, size_t column_position, size_t line_number);
