012e5533081a7c57171f47a3b72d02a712703b4f
sk <content.type.text.net@gmail.com>
2021-11-02 00:12:46 +0900

    記号なのか文字なのか頭バグりそうなので名前変更

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.z.token.h
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.z.token.h
@@ -3,28 +3,40 @@
 
 #include "token.h"
 
-typedef struct tag_SINGLE_CHAR_TOKEN
+typedef struct tag_SINGLE_SYMBOL_TOKEN
 {
-    TCHAR character;
+    TCHAR symbol;
     TOKEN_KIND kind;
-} SINGLE_CHAR_TOKEN;
+} SINGLE_SYMBOL_TOKEN;
 
-//#define MULTI_CHAR_TOKEN_FIRST (0)
-//#define MULTI_CHAR_TOKEN_SECOND (1)
-#define MULTI_CHAR_TOKEN_COUNT (4)
-typedef struct tag_MULTI_CHAR_TOKEN
+#define MULTI_CHAR_SYMBOL_COUNT (4)
+typedef struct tag_MULTI_SYMBOL_TOKEN
 {
-    TCHAR characters[MULTI_CHAR_TOKEN_COUNT];
+    TCHAR symbols[MULTI_CHAR_SYMBOL_COUNT];
     /// <summary>
-    /// MULTI_CHAR_TOKEN_FIRST: characters[MULTI_CHAR_TOKEN_FIRST] のみのトークン
-    /// MULTI_CHAR_TOKEN_SECOND: characters[MULTI_CHAR_TOKEN_FIRST][MULTI_CHAR_TOKEN_SECOND]のトークン
+    /// 順々のトークン種別。
     /// </summary>
-    TOKEN_KIND kinds[MULTI_CHAR_TOKEN_COUNT];
+    /// <list type="number>
+    ///     <listheader>
+    ///         <term>【定義見出し・見出し】</term>
+    ///         <description>【説明見出し】</description>
+    ///     </listheader>
+    ///     <item>
+    ///         <description>symbols[0] のみのトークン</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>symbols[0][1]のトークン</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>symbols[0][1][N..]のトークン</description>
+    ///     </item>
+    /// </list>
+    TOKEN_KIND kinds[MULTI_CHAR_SYMBOL_COUNT];
     /// <summary>
     /// 真の場合にコメント中は無視する
     /// </summary>
-    bool skip_comments[MULTI_CHAR_TOKEN_COUNT];
-} MULTI_CHAR_TOKEN;
+    bool skip_comments[MULTI_CHAR_SYMBOL_COUNT];
+} MULTI_SYMBOL_TOKEN;
 
 /// <summary>
 /// 指定トークンはコメントか。
@@ -34,20 +46,20 @@ typedef struct tag_MULTI_CHAR_TOKEN
 bool is_comment_token_kind(TOKEN_KIND kind);
 
 /// <summary>
-/// 単一文字で構成されるトークンを取得。
+/// 単一記号で構成されるトークンを取得。
 /// </summary>
 /// <param name="c"></param>
 /// <returns>取得できたトークン設定。取得できない場合は<c>NULL</c></returns>
-const SINGLE_CHAR_TOKEN* find_single_character_token(TCHAR c);
+const SINGLE_SYMBOL_TOKEN* find_single_symbol_token(TCHAR c);
 
 /// <summary>
-/// 複数文字で構成されるトークンを読み込み。
+/// 複数記号で構成されるトークンを読み込み。
 /// </summary>
 /// <param name="token_result">結果格納</param>
 /// <param name="source">ソース全体</param>
-/// <param name="start_index">複数文字トークンとしての開始点</param>
+/// <param name="start_index">複数記号トークンとしての開始点</param>
 /// <param name="last_token_kind">前回トークン種別</param>
 /// <param name="source_position"></param>
 /// <param name="project_setting"></param>
 /// <returns>読み込み成功後に飛ばす長さ(start_indexからの相対位置)。0の場合は失敗しているので後続不要。</returns>
-size_t read_multi_character_token(TOKEN_RESULT* token_result, const TEXT* source, size_t start_index, TOKEN_KIND last_token_kind, const SOURCE_POSITION* source_position, const PROJECT_SETTING* project_setting);
+size_t read_multi_symbol_token(TOKEN_RESULT* token_result, const TEXT* source, size_t start_index, TOKEN_KIND last_token_kind, const SOURCE_POSITION* source_position, const PROJECT_SETTING* project_setting);
