9b32cd89f1e82ba47040b5d5155ad49288fd259d
sk <content.type.text.net@gmail.com>
2021-11-01 14:59:21 +0900

    行番号と桁位置を最終的オブジェクトである構造体として持ち運ぶようにした

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.z.string.h
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.z.string.h
@@ -9,4 +9,4 @@
 /// <param name="source">ソース全体</param>
 /// <param name="start_index">文字列トークンとしての開始点</param>
 /// <returns>読み込み成功後に飛ばす長さ(start_indexからの相対位置)。0の場合は失敗しているので後続不要。</returns>
-size_t read_string_token(TOKEN_RESULT* token_result, const TEXT* source, size_t start_index, size_t column_position, size_t line_number, const PROJECT_SETTING* project_setting);
+size_t read_string_token(TOKEN_RESULT* token_result, const TEXT* source, size_t start_index, const SOURCE_POSITION* source_position, const PROJECT_SETTING* project_setting);
