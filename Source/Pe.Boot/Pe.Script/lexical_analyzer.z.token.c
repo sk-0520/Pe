3859ebf64fe254f2bf235219c9132f3f34497bf3
sk <content.type.text.net@gmail.com>
2021-11-01 23:08:29 +0900

    今後3文字以上の記号組み合わせトークンがあるかもしれないので一応対応だけしておく

--- Source/Pe.Boot/Pe.Script/lexical_analyzer.z.token.c
+++ Source/Pe.Boot/Pe.Script/lexical_analyzer.z.token.c
@@ -1,4 +1,6 @@
-﻿#include "lexical_analyzer.h"
+﻿#include "../Pe.Library/common.h"
+
+#include "lexical_analyzer.h"
 #include "lexical_analyzer.z.token.h"
 
 static SINGLE_CHAR_TOKEN library__single_character_tokens[] = {
@@ -68,102 +70,106 @@ static SINGLE_CHAR_TOKEN library__single_character_tokens[] = {
     },
 };
 
-static MULTI_CHAR_TOKEN library__multi_tokens[] = {
+#define NONE_CHAR_SKIP_2 '\0', '\0'
+#define NONE_KIND_SKIP_2 TOKEN_KIND_NONE, TOKEN_KIND_NONE
+#define COMMENT_ALL_SKIP true, true, true, true
+
+static MULTI_CHAR_TOKEN library__multi_character_tokens[] = {
     // +
     {
-        .characters = { '+', '=' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_ADD_ASSIGN },
-        .skip_comments = { true, true },
+        .characters = { '+', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_ADD_ASSIGN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '+', '+' },
-        .kinds = { TOKEN_KIND_OP_PLUS, TOKEN_KIND_OP_INCREMENT },
-        .skip_comments = { true, true },
+        .characters = { '+', '+', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_PLUS, TOKEN_KIND_OP_INCREMENT, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // -
     {
-        .characters = { '-', '=' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_SUB_ASSIGN },
-        .skip_comments = { true, true },
+        .characters = { '-', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_SUB_ASSIGN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '-', '-' },
-        .kinds = { TOKEN_KIND_OP_MINUS, TOKEN_KIND_OP_DECREMENT },
-        .skip_comments = { true, true },
+        .characters = { '-', '-', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_MINUS, TOKEN_KIND_OP_DECREMENT, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // *
     {
-        .characters = { '*', '=' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_MUL_ASSIGN },
-        .skip_comments = { true, true },
+        .characters = { '*', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_MUL_ASSIGN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '*', '/' },
-        .kinds = { TOKEN_KIND_OP_STAR, TOKEN_KIND_COMMENT_BLOCK_END },
-        .skip_comments = { true, false },
+        .characters = { '*', '/', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_STAR, TOKEN_KIND_COMMENT_BLOCK_END, NONE_KIND_SKIP_2, },
+        .skip_comments = { true, false, true, true, },
     },
     // /
     {
-        .characters = { '/', '/' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_COMMENT_LINE },
-        .skip_comments = { false, false },
+        .characters = { '/', '/', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_COMMENT_LINE, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '/', '*' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_COMMENT_BLOCK_BEGIN },
-        .skip_comments = { false, false },
+        .characters = { '/', '*', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_COMMENT_BLOCK_BEGIN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '/', '=' },
-        .kinds = { TOKEN_KIND_OP_SLASH, TOKEN_KIND_OP_DIV_ASSIGN },
-        .skip_comments = { false, false },
+        .characters = { '/', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_SLASH, TOKEN_KIND_OP_DIV_ASSIGN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // %
     {
-        .characters = { '%', '=' },
-        .kinds = { TOKEN_KIND_OP_PERCENT, TOKEN_KIND_OP_REM_ASSIGN },
-        .skip_comments = { false, false },
+        .characters = { '%', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_PERCENT, TOKEN_KIND_OP_REM_ASSIGN, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // =
     {
-        .characters = { '=', '>' },
-        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_LAMBDA },
-        .skip_comments = { false, false },
+        .characters = { '=', '>', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_NONE, TOKEN_KIND_OP_LAMBDA, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     {
-        .characters = { '=', '=' },
-        .kinds = { TOKEN_KIND_OP_ASSIGN, TOKEN_KIND_OP_EQUALS },
-        .skip_comments = { false, false },
+        .characters = { '=', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_ASSIGN, TOKEN_KIND_OP_EQUALS, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // <
     {
-        .characters = { '<', '=' },
-        .kinds = { TOKEN_KIND_OP_LESS, TOKEN_KIND_OP_LESS_EQUAL },
-        .skip_comments = { false, false },
+        .characters = { '<', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_LESS, TOKEN_KIND_OP_LESS_EQUAL, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // >
     {
-        .characters = { '>', '=' },
-        .kinds = { TOKEN_KIND_OP_GREATER, TOKEN_KIND_OP_GREATER_EQUAL },
-        .skip_comments = { false, false },
+        .characters = { '>', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_GREATER, TOKEN_KIND_OP_GREATER_EQUAL, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // !
     {
-        .characters = { '!', '=' },
-        .kinds = { TOKEN_KIND_OP_EXCLAMATION, TOKEN_KIND_OP_NOT_EQUALS },
-        .skip_comments = { false, false },
+        .characters = { '!', '=', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_EXCLAMATION, TOKEN_KIND_OP_NOT_EQUALS, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // &
     {
-        .characters = { '&', '&' },
-        .kinds = { TOKEN_KIND_OP_AMPERSAND, TOKEN_KIND_OP_AND },
-        .skip_comments = { false, false },
+        .characters = { '&', '&', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_AMPERSAND, TOKEN_KIND_OP_AND, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
     // |
     {
-        .characters = { '|', '|' },
-        .kinds = { TOKEN_KIND_OP_VERTICALBAR, TOKEN_KIND_OP_OR },
-        .skip_comments = { false, false },
+        .characters = { '|', '|', NONE_CHAR_SKIP_2, },
+        .kinds = { TOKEN_KIND_OP_VERTICALBAR, TOKEN_KIND_OP_OR, NONE_KIND_SKIP_2, },
+        .skip_comments = { COMMENT_ALL_SKIP },
     },
 };
 
@@ -189,22 +195,38 @@ const SINGLE_CHAR_TOKEN* find_single_character_token(TCHAR c)
 
 size_t read_multi_character_token(TOKEN_RESULT* token_result, const TEXT* source, size_t start_index, TOKEN_KIND last_token_kind, const SOURCE_POSITION* source_position, const PROJECT_SETTING* project_setting)
 {
-    TCHAR current_character = source->value[start_index];
-    TCHAR next_character = get_next_character(source, start_index);
+    TCHAR characters[MULTI_CHAR_TOKEN_COUNT];
+    const size_t characters_length = sizeof(characters) / sizeof(characters[0]);
+    for (size_t i = 0; i < characters_length; i++) {
+        characters[i] = get_relative_character(source, start_index, i);
+    }
 
-    for (size_t i = 0; i < sizeof(library__multi_tokens) / sizeof(library__multi_tokens[0]); i++) {
-        const MULTI_CHAR_TOKEN* multi_token = library__multi_tokens + i;
-        if (current_character == multi_token->characters[MULTI_CHAR_TOKEN_FIRST]) {
-            if (next_character && next_character == multi_token->characters[MULTI_CHAR_TOKEN_SECOND] && multi_token->kinds[MULTI_CHAR_TOKEN_SECOND] != TOKEN_KIND_NONE) {
-                if (!is_comment_token_kind(last_token_kind) || !multi_token->skip_comments[MULTI_CHAR_TOKEN_SECOND]) {
-                    add_token_kind(&token_result->token, multi_token->kinds[MULTI_CHAR_TOKEN_SECOND], source_position);
-                }
-                return 2;
-            } else if (multi_token->kinds[MULTI_CHAR_TOKEN_FIRST] != TOKEN_KIND_NONE) {
-                if (!is_comment_token_kind(last_token_kind) || !multi_token->skip_comments[MULTI_CHAR_TOKEN_FIRST]) {
-                    add_token_kind(&token_result->token, multi_token->kinds[MULTI_CHAR_TOKEN_FIRST], source_position);
-                }
-                return 1;
+    for (size_t i = 0; i < sizeof(library__multi_character_tokens) / sizeof(library__multi_character_tokens[0]); i++) {
+        const MULTI_CHAR_TOKEN* multi_token = library__multi_character_tokens + i;
+
+        // 一文字目が合致しないのであればどう頑張っても比較する意味がない
+        if (multi_token->characters[0] != characters[0]) {
+            continue;
+        }
+
+        // 終端から最初に向かって戻していく
+        for (size_t j = characters_length; j; j--) {
+            size_t index = j - 1;
+            if (!characters[index] || multi_token->kinds[index] == TOKEN_KIND_NONE) {
+                continue;
+            }
+
+            bool all_characters_match = true;
+            for (size_t k = 0; all_characters_match && k < j; k++) {
+                all_characters_match &= characters[k] == multi_token->characters[k];
+            }
+            if (!all_characters_match) {
+                continue;
+            }
+
+            if (!is_comment_token_kind(last_token_kind) || !multi_token->skip_comments[index]) {
+                add_token_kind(&token_result->token, multi_token->kinds[index], source_position);
+                return j;
             }
         }
     }
