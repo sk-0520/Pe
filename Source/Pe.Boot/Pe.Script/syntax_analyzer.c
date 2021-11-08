4cd9e5567e9cff3e3db486b8379e9b75fd83b10d
sk <content.type.text.net@gmail.com>
2021-11-09 00:15:21 +0900

    ほら、こういう構造のなんか作ればいいんでしょ！ インターネットでみた！ しらんけど！！

---
+++ Source/Pe.Boot/Pe.Script/syntax_analyzer.c
@@ -0,0 +1,38 @@
+﻿#include "syntax_analyzer.h"
+
+SYNTAX_DEFINES aaa = {
+    .defines = {
+        {
+            .kind = SYNTAX_DEFINE_KIND_EXPRESSIONS_IF_EXISTS,
+            .elements = {
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_OP_COMMA }, { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 2 },
+            },
+            .length = 1,
+        },
+        {
+            .kind = SYNTAX_DEFINE_KIND_EXPRESSION,
+            .elements = {
+                { { { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION } }, 1 },
+                { { { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_TYPE_TOKEN, TOKEN_KIND_OP_PLUS }, { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+                { { { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_TYPE_TOKEN, TOKEN_KIND_OP_MINUS }, { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+                { { { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_TYPE_TOKEN, TOKEN_KIND_OP_STAR }, { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+                { { { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_TYPE_TOKEN, TOKEN_KIND_OP_SLASH }, { SYNTAX_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+            },
+            .length = 5,
+        },
+        {
+            .kind = SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION,
+            .elements = {
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_LITERAL_INTEGER } }, 1 },
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_LITERAL_DECIMAL } }, 1 },
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_LITERAL_SSTRING } }, 1 },
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_LITERAL_DSTRING } }, 1 },
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_LITERAL_BSTRING } }, 1 },
+                { { { SYNTAX_TYPE_TOKEN, TOKEN_KIND_WORD } }, 1 },
+            },
+            .length = 6,
+        },
+    },
+    .length = 2,
+};
+
---
+++ Source/Pe.Boot/Pe.Script/syntax_analyzer.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <stddef.h>
+
+#include "lexical_analyzer.h"
+#include "lexical_analyzer.z.token.h"
+//#include "lexical_analyzer.z.string.h"
+//#include "lexical_analyzer.z.number.h"
+//#include "lexical_analyzer.z.word.h"
+
+typedef enum tag_SYNTAX_DEFINE_KIND
+{
+    SYNTAX_DEFINE_KIND_NONE,
+    /// <summary>
+    /// [{, expr}*]
+    /// </summary>
+    SYNTAX_DEFINE_KIND_EXPRESSIONS_IF_EXISTS,
+    /// <summary>
+    /// SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION
+    /// expr TOKEN_KIND_OP_PLUS expr
+    /// expr TOKEN_KIND_OP_MINUS expr
+    /// expr TOKEN_KIND_OP_STAR expr
+    /// expr TOKEN_KIND_OP_SLASH expr
+    /// expr TOKEN_KIND_OP_PERCENT expr
+    /// </summary>
+    SYNTAX_DEFINE_KIND_EXPRESSION,
+    /// <summary>
+    /// TOKEN_KIND_WORD
+    /// TOKEN_KIND_LITERAL_INTEGER
+    /// TOKEN_KIND_LITERAL_DECIMAL
+    /// TOKEN_KIND_LITERAL_SSTRING
+    /// TOKEN_KIND_LITERAL_DSTRING
+    /// TOKEN_KIND_LITERAL_BSTRING
+    /// </summary>
+    SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION,
+} SYNTAX_DEFINE_KIND;
+
+typedef enum tag_SYNTAX_TYPE
+{
+    SYNTAX_TYPE_TOKEN,
+    SYNTAX_TYPE_DEFINE,
+    //SYNTAX_TYPE_ELEMENTS_IF_EXISTS,
+} SYNTAX_TYPE;
+
+typedef struct tag_SYNTAX_ELEMENT
+{
+    SYNTAX_TYPE type;
+    union
+    {
+        SYNTAX_DEFINE_KIND define;
+        TOKEN_KIND token;
+    } item;
+} SYNTAX_ELEMENT;
+
+typedef struct tag_SYNTAX_ELEMENTS
+{
+    SYNTAX_ELEMENT data[16]; // 自動生成作って動的にする想定
+    size_t length;
+} SYNTAX_ELEMENTS;
+
+typedef struct tag_SYNTAX_DEFINE
+{
+    SYNTAX_DEFINE_KIND kind;
+    SYNTAX_ELEMENTS elements[128];
+    size_t length;
+} SYNTAX_DEFINE;
+
+typedef struct tag_SYNTAX_DEFINES
+{
+    SYNTAX_DEFINE defines[128];
+    size_t length;
+} SYNTAX_DEFINES;
+
+
