99e39a2f415bdcff01aad785985e19bd82b7f044
sk <content.type.text.net@gmail.com>
2021-11-09 15:34:31 +0900

    何がしたいのか

---
+++ Source/Pe.Boot/Pe.Script.Test/syntax_analyzer.test.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+
+extern "C" {
+#   include "../Pe.Script/syntax_analyzer.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ScriptTest
+{
+    TEST_CLASS(lexical_analyzer_test)
+    {
+    public:
+        TEST_METHOD(test)
+        {
+            PROJECT_SETTING setting;
+
+            TEXT source = wrap("123");
+            TOKEN_RESULT token_result = analyze_lexical(NULL, &source, &setting);
+            analyze_syntax(&token_result, &setting);
+        }
+
+    };
+}
