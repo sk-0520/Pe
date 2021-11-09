27fcda234c82003452400f6558eb713e64660d32
sk <content.type.text.net@gmail.com>
2021-11-09 23:08:39 +0900

    SYNTAX_DEFINES はいらんでしょ

--- Source/Pe.Boot/Pe.Script/syntax_analyzer.c
+++ Source/Pe.Boot/Pe.Script/syntax_analyzer.c
@@ -3,47 +3,44 @@
 
 #include "syntax_analyzer.h"
 
-SYNTAX_DEFINES script__syntax_define = {
-    .defines = {
-        {
-            .kind = SYNTAX_DEFINE_KIND_MORE_ARGUMENTS,
-            .elements = {
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_COMMA }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 2 },
-            },
-            .length = 1,
+SYNTAX_DEFINE script__syntax_defines[] = {
+    {
+        .kind = SYNTAX_DEFINE_KIND_MORE_ARGUMENTS,
+        .elements = {
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_COMMA }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 2 },
         },
-        {
-            .kind = SYNTAX_DEFINE_KIND_MORE_ARGUMENTS,
-            .elements = {
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_MORE_0, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 2 },
-            },
-            .length = 1,
-        },
-        {
-            .kind = SYNTAX_DEFINE_KIND_EXPRESSION,
-            .elements = {
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_PLUS }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_MINUS }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_STAR }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_SLASH }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
-            },
-            .length = 5,
-        },
-        {
-            .kind = SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION,
-            .elements = {
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_INTEGER } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_DECIMAL } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_SSTRING } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_DSTRING } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_BSTRING } }, 1 },
-                { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_WORD } }, 1 },
-            },
-            .length = 6,
-        },
+        .length = 1,
     },
-    .length = 2,
+    {
+        .kind = SYNTAX_DEFINE_KIND_MORE_ARGUMENTS,
+        .elements = {
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_MORE_0, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 2 },
+        },
+        .length = 1,
+    },
+    {
+        .kind = SYNTAX_DEFINE_KIND_EXPRESSION,
+        .elements = {
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_PLUS }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_MINUS }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_STAR }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_OP_SLASH }, { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_DEFINE, SYNTAX_DEFINE_KIND_EXPRESSION } }, 3 },
+        },
+        .length = 5,
+    },
+    {
+        .kind = SYNTAX_DEFINE_KIND_PRIMARY_EXPRESSION,
+        .elements = {
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_INTEGER } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_DECIMAL } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_SSTRING } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_DSTRING } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_LITERAL_BSTRING } }, 1 },
+            { { { SYNTAX_RULE_NORMAL, SYNTAX_ELEMENT_TYPE_TOKEN, TOKEN_KIND_WORD } }, 1 },
+        },
+        .length = 6,
+    },
 };
 
 void analyze_syntax(const TOKEN_RESULT* token_result, const PROJECT_SETTING* setting)
@@ -52,10 +49,10 @@ void analyze_syntax(const TOKEN_RESULT* token_result, const PROJECT_SETTING* set
     size_t current_index = 0;
     while (current_index < token_result->token.length) {
         const TOKEN* token = tokens + current_index++;
-        
-        for (size_t i = 0; i < script__syntax_define.length; i++) {
-            for (size_t j = 0; i < script__syntax_define.defines[i].length; j++) {
-                const SYNTAX_ELEMENTS* elements = script__syntax_define.defines[i].elements + j;
+
+        for (size_t i = 0; i < SIZEOF_ARRAY(script__syntax_defines); i++) {
+            for (size_t j = 0; i < script__syntax_defines[i].length; j++) {
+                const SYNTAX_ELEMENTS* elements = script__syntax_defines[i].elements + j;
                 for (size_t k = 0; k < elements->length; k++) {
                     const SYNTAX_ELEMENT* element = elements->data + i;
                     switch (element->type) {
