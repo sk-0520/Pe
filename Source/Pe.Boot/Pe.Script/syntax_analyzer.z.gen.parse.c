3994da1cb1790b0308ca0e1468ccca5e7e208774
sk <content.type.text.net@gmail.com>
2021-11-28 20:18:45 +0900

    つらつら

--- Source/Pe.Boot/Pe.Script/syntax_analyzer.z.gen.parse.c
+++ Source/Pe.Boot/Pe.Script/syntax_analyzer.z.gen.parse.c
@@ -3,7 +3,7 @@
 #include "../Pe.Library/logging.h"
 #include "../Pe.Library/memory.h"
 
-#include "syntax_analyzer.z.default_parse.h"
+#include "syntax_analyzer.z.parse.h"
 
 #define DEFAULT_SYNTAX_DEFINE_LENGTH (5)
 
@@ -312,20 +312,6 @@ SYNTAXES new_default_syntaxes(void)
     syntax_defines[4] = define_4_factor;
 
     return result;
-}
-
-void release_syntaxes(SYNTAXES* syntaxes)
-{
-    const MEMORY_RESOURCE* memory_resource = syntaxes->script.memory_resource;
-
-    for (size_t i = 0; i < syntaxes->length; i++) {
-        for (size_t j = 0; j < syntaxes->defines[i].length; j++) {
-            release_memory(syntaxes->defines[i].elements[j].data, memory_resource);
-        }
-        release_memory(syntaxes->defines[i].elements, memory_resource);
-    }
-    release_memory(syntaxes->defines, memory_resource);
-    set_memory(syntaxes, sizeof(SYNTAXES), 0);
 }
 
 
