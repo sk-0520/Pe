94cb35440c864031f0d0c01309dc0ec79bf7215a
sk <content.type.text.net@gmail.com>
2022-12-28 21:28:02 +0900

    C17 追従

--- Source/Pe.Boot/Pe.Script.Test/test.cpp
+++ Source/Pe.Boot/Pe.Script.Test/test.cpp
@@ -8,23 +8,23 @@ extern "C" {
 TestImpl TEST(tstring(_T("ScriptTest")));
 
 #ifdef SCRIPT_MEMORY_RESOURCE
-static MEMORY_RESOURCE script_memory_resource;
+static MEMORY_ARENA_RESOURCE script_memory_arena_resource;
 #endif
 
 TEST_MODULE_INITIALIZE(initialize)
 {
 #ifdef SCRIPT_MEMORY_RESOURCE
-    script_memory_resource = new_memory_resource(0, 0);
-    set_script_memory_resource(&script_memory_resource);
+    script_memory_arena_resource = new_memory_resource(0, 0);
+    set_script_memory_arena_resource(&script_memory_arena_resource);
 #else
-    set_script_memory_resource(DEFAULT_MEMORY);
+    set_script_memory_arena_resource(DEFAULT_MEMORY_ARENA);
 #endif
 }
 
 TEST_MODULE_CLEANUP(cleanup)
 {
 #ifdef SCRIPT_MEMORY_RESOURCE
-    release_memory_resource(&script_memory_resource);
+    release_memory_arena_resource(&script_memory_arena_resource);
 #endif
 }
 
