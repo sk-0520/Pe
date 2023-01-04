0783649147be77fd60e0149f228823979453eb1d
sk <content.type.text.net@gmail.com>
2023-01-04 21:25:21 +0900

    __ 追従

--- Source/Pe.Boot/Pe.Script/script.c
+++ Source/Pe.Boot/Pe.Script/script.c
@@ -1,12 +1,12 @@
 ﻿#include "script.h"
 
-static const MEMORY_ARENA_RESOURCE* script__memory_arena_resource;
+static const MEMORY_ARENA_RESOURCE* script_memory_arena_resource;
 
 void set_script_memory_arena_resource(const MEMORY_ARENA_RESOURCE* memory_arena_resource)
 {
-    script__memory_arena_resource = memory_arena_resource;
+    script_memory_arena_resource = memory_arena_resource;
 }
 const MEMORY_ARENA_RESOURCE* get_script_memory_arena_resource()
 {
-    return script__memory_arena_resource;
+    return script_memory_arena_resource;
 }
