8b61bc539803f836c37c961908e239a9ae9f4095
sk <content.type.text.net@gmail.com>
2023-01-17 20:22:59 +0900

    どういう構造であれこれすりゃいいんだろうなぁ

--- Source/Pe.Boot/Pe.Boot/gui_test.c
+++ Source/Pe.Boot/Pe.Boot/gui_test.c
@@ -1,6 +1,10 @@
 ﻿#include "gui_test.h"
 
-void gui_test()
+void gui_test(HINSTANCE hInstance)
 {
+    MEMORY_ARENA_RESOURCE memory_arena_resource = new_memory_arena_resource(MEMORY_ARENA_AUTO_INITIAL_SIZE, MEMORY_ARENA_EXTENDABLE_MAXIMUM_SIZE);
+    GUI_ROOT_RESOURCE gcr = create_gui_root_resource(hInstance, &memory_arena_resource);
 
+    release_gui_root_resource(&gcr);
+    release_memory_arena_resource(&memory_arena_resource);
 }
