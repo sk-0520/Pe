4220b4946b9b65047d70bd845f92063b03c006f5
sk <content.type.text.net@gmail.com>
2023-01-17 22:05:45 +0900

    別に何かする気はないけど一応テストの追加

---
+++ Source/Pe.Boot/Pe.Gui.Test/abc.test.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+
+extern "C" {
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PeGuiTest
+{
+    TEST_CLASS(abc_test)
+    {
+    public:
+        TEST_METHOD(xyz_test)
+        {
+            Assert::IsTrue(true);
+        }
+    };
+}
