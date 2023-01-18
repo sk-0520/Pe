a8e34478f82eddeebdb68508093318c45c6ba34a
sk <content.type.text.net@gmail.com>
2023-01-18 17:43:17 +0900

    Cだけでやる場合、隠せないものが多い気がするような気がする

--- Source/Pe.Boot/Pe.Gui/component.c
+++ Source/Pe.Boot/Pe.Gui/component.c
@@ -1,4 +1,15 @@
-﻿
+﻿#include "../Pe.Library/text.h"
+#include "component.h"
+
+static WNDCLASS create_component_core(COMPONENT_BASE* result, COMPONENT_TYPE component_type, const TEXT* custom_class_name)
+{
+    WNDCLASS wnd_class /*= {
+    }*/;
+
+
+    return wnd_class;
+}
+
 void create_component()
 {
 
