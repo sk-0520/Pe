4c792fcd5a800088cf1b29e012445a2dffba909c
sk <content.type.text.net@gmail.com>
2023-03-06 20:38:07 +0900

    これはあかんは

---
+++ Source/Pe/Pe.Standard.Database/Migrations/IMigrationItem.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentTypeTextNet.Pe.Standard.Database.Migrations
+{
+    public interface IMigrationItem
+    {
+    }
+}
