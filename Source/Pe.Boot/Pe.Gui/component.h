538562f374986aa7cf897396b7d0cd1b05388cc3
sk <content.type.text.net@gmail.com>
2023-06-08 20:30:33 +0900

    あー、つら

--- Source/Pe.Boot/Pe.Gui/component.h
+++ Source/Pe.Boot/Pe.Gui/component.h
@@ -22,14 +22,35 @@ typedef enum tag_COMPONENT_TYPE
 
 typedef struct tag_GUI_COMPONENT_LIBRARY
 {
-    const COMPONENT_TYPE type;
     const HANDLE handle;
     const GUI_ROOT_RESOURCE* root;
+    const COMPONENT_TYPE type;
+    const WNDCLASS wnd_class;
 } GUI_COMPONENT_LIBRARY;
 
-typedef struct tag_COMPONENT_BASE
+//typedef struct tag_COMPONENT_BASE
+//{
+//    GUI_COMPONENT_LIBRARY library;
+//} COMPONENT_BASE;
+
+typedef struct tag_WINDOW_COMPONENT
 {
     GUI_COMPONENT_LIBRARY library;
-} COMPONENT_BASE;
+} WINDOW_COMPONENT;
 
-void create_component();
+typedef enum tag_WINDOW_COMPONENT_STYLE
+{
+
+} WINDOW_COMPONENT_STYLE;
+
+typedef struct tag_WINDOW_COMPONENT_OPTIONS
+{
+    HINSTANCE hInstance;
+    const WNDPROC procedure;
+    const TEXT* window_class_name;
+    const TEXT* title;
+    WINDOW_COMPONENT_STYLE style;
+    bool visible;
+} WINDOW_COMPONENT_OPTIONS;
+
+WINDOW_COMPONENT create_window_component(const WINDOW_COMPONENT_OPTIONS* options);
