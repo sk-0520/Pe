6c2f8bc49d41e8bf4bbc16f0af25c7779c2b85e5
sk <content.type.text.net@gmail.com>
2023-06-08 20:30:33 +0900

    あー、つら

--- Source/Pe.Boot/Pe.Boot/gui_test.c
+++ Source/Pe.Boot/Pe.Boot/gui_test.c
@@ -5,7 +5,7 @@ void gui_test(HINSTANCE hInstance)
     MEMORY_ARENA_RESOURCE memory_arena_resource = new_memory_arena_resource(MEMORY_ARENA_AUTO_INITIAL_SIZE, MEMORY_ARENA_EXTENDABLE_MAXIMUM_SIZE);
     GUI_ROOT_RESOURCE gcr = create_gui_root_resource(hInstance, &memory_arena_resource);
 
-    //loop_message_gui(NULL, &gcr);
+    loop_message_gui(NULL, &gcr);
 
     release_gui_root_resource(&gcr);
     release_memory_arena_resource(&memory_arena_resource);
