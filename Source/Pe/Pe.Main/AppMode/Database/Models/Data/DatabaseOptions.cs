df7ff8378392554f9f88bafd43a483466cd236ae
sk <content.type.text.net@gmail.com>
2023-06-10 21:39:04 +0900

    自分でもびっくりしたけど複数値サポートしてないんか・・・

--- Source/Pe/Pe.Main/AppMode/Database/Models/Data/DatabaseOptions.cs
+++ Source/Pe/Pe.Main/AppMode/Database/Models/Data/DatabaseOptions.cs
@@ -1,12 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Standard.Base;
 
 namespace ContentTypeTextNet.Pe.Main.AppMode.Database.Models.Data
 {
     internal class DatabaseOptions
     {
+        #region property
+
+        [CommandLine(longKey:"database")]
+        public string Database { get; set; } = string.Empty;
+
+        #endregion
     }
 }
