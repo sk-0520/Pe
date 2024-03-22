d907581cc85748a879bc06d1f1f31e6201cc4089
sk <content.type.text.net@gmail.com>
2024-03-22 19:47:35 +0900

    VsLocalizedIntellisense から command 移植

---
+++ Source/Pe/Pe.Mvvm/Command/CommandBase.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ContentTypeTextNet.Pe.Mvvm.Command
+{
+    /// <summary>
+    /// <see cref="ICommand"/>実装基底クラス。
+    /// </summary>
+    public abstract class CommandBase: ICommand
+    {
+        protected CommandBase()
+        {
+            SynchronizationContext = SynchronizationContext.Current;
+        }
+
+        #region property
+
+        protected SynchronizationContext? SynchronizationContext { get; }
+
+        /// <summary>
+        /// 同時実行を抑制するか。
+        /// <para>基本的に <see langword="init"/> であることを前提としてる。使えんけど。</para>
+        /// </summary>
+        public bool SuppressCommandWhileExecuting { get; set; } = true;
+
+        #endregion
+
+        #region function
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            if(SynchronizationContext != null && SynchronizationContext != SynchronizationContext.Current) {
+                SynchronizationContext.Post(o => CanExecuteChanged?.Invoke(this, EventArgs.Empty), null);
+            } else {
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged();
+        }
+
+        #endregion
+
+        #region ICommand
+
+        public event EventHandler? CanExecuteChanged;
+
+        public abstract void Execute(object? parameter);
+
+        public abstract bool CanExecute(object? parameter);
+
+        #endregion
+    }
+
+}
