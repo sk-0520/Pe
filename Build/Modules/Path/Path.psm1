ba84da741cf8f98f07956a249682e9abf6b3f191
sk <content.type.text.net@gmail.com>
2024-04-18 20:20:53 +0900

    path: Invoke-Location

---
+++ Build/Modules/Path/Path.psm1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+<#
+.SYNOPSIS
+	IDisposable に対する自動解放処理。
+
+.PARAMETER Path
+	IDisposable オブジェクト。
+
+.PARAMETER ScriptBlock
+	処理。
+
+#>
+function Invoke-Location {
+	Param(
+		[Parameter(mandatory = $true, Position = 0)][string] $Path,
+		[Parameter(mandatory = $true, Position = 1)][ScriptBlock] $ScriptBlock
+	)
+
+	Push-Location -LiteralPath $Path
+	try {
+		Invoke-Command -ScriptBlock $ScriptBlock
+	} finally {
+		Pop-Location
+	}
+}
