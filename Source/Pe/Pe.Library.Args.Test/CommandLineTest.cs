8c01480238616f199a898e37295e5d88b1674f63
sk <content.type.text.net@gmail.com>
2024-11-10 15:05:14 +0900

    復帰できる分だけ単純復帰

--- Source/Pe/Pe.Library.Args.Test/CommandLineTest.cs
+++ Source/Pe/Pe.Library.Args.Test/CommandLineTest.cs
@@ -7,117 +7,111 @@ using Xunit;
 
 namespace ContentTypeTextNet.Pe.Library.Args.Test
 {
-    //public class CommandLineTest
-    //{
-    //    [Theory]
-    //    [InlineData(new[] { "a" }, true, "a", 0)]
-    //    [InlineData(new[] { "A", "a" }, true, "A", 1)]
-    //    [InlineData(new[] { "A", "a" }, false, "", 2)]
-    //    [InlineData(new string[] { }, true, "", 0)]
-    //    [InlineData(new string[] { }, false, "", 0)]
-    //    public void ConstructorTest(string[] args, bool withCommand, string expectedProgramName, int expectedArgumentCount)
-    //    {
-    //        var commandLine = new CommandLine(args, withCommand);
-    //        Assert.True(commandLine.CommandName == expectedProgramName);
-    //        Assert.True(commandLine.Arguments.Count == expectedArgumentCount);
-    //    }
+    public class CommandLineTest
+    {
+        [Theory]
+        [InlineData(new[] { "a" }, true, "a", 0)]
+        [InlineData(new[] { "A", "a" }, true, "A", 1)]
+        [InlineData(new[] { "A", "a" }, false, "", 2)]
+        [InlineData(new string[] { }, true, "", 0)]
+        [InlineData(new string[] { }, false, "", 0)]
+        public void ConstructorTest(string[] args, bool withCommand, string expectedProgramName, int expectedArgumentCount)
+        {
+            var commandLine = new CommandLine(args, withCommand);
+            Assert.True(commandLine.CommandName == expectedProgramName);
+            Assert.True(commandLine.Arguments.Count == expectedArgumentCount);
+        }
 
-    //    [Theory]
-    //    [InlineData(true, 'a', "aa")]
-    //    [InlineData(true, '\0', "aa")]
-    //    [InlineData(true, 'a', "")]
-    //    [InlineData(false, '\0', "")]
-    //    [InlineData(false, '\0', "a")]
-    //    public void AddTest(bool expected, char shortKey, string longKey)
-    //    {
-    //        var commandLine = new CommandLine();
-    //        try {
-    //            var key = commandLine.Add(shortKey, longKey);
-    //            Assert.True(expected);
-    //        } catch(ArgumentException ex) {
-    //            Assert.False(expected, ex.ToString());
-    //        }
-    //    }
+        //TODO: 例外と分けた方がいい
+        [Theory]
+        [InlineData(true, "aa")]
+        [InlineData(false, "")]
+        [InlineData(false, "a")]
+        public void AddTest(bool expected, string longKey)
+        {
+            var commandLine = new CommandLine();
+            try {
+                var key = commandLine.Add(longKey);
+                Assert.True(expected);
+            } catch(ArgumentException ex) {
+                Assert.False(expected, ex.ToString());
+            }
+        }
+        //TOOD: AddTest の観点と同じ
+        [Theory]
+        [InlineData(false, "")]
+        [InlineData(false, "aa")]
+        [InlineData(false, "bb")]
+        [InlineData(true, "aaa")]
+        public void AddTest_Exists(bool expected, string longKey)
+        {
+            var commandLine = new CommandLine();
+            commandLine.Add("aa");
+            commandLine.Add("bb");
 
-    //    [Theory]
-    //    [InlineData(false, 'a', "")]
-    //    [InlineData(false, '\0', "aa")]
-    //    [InlineData(false, '\0', "bb")]
-    //    [InlineData(false, 'c', "")]
-    //    [InlineData(true, 'b', "aaa")]
-    //    public void AddTest_Exists(bool expected, char shortKey, string longKey)
-    //    {
-    //        var commandLine = new CommandLine();
-    //        commandLine.Add('a', "aa");
-    //        commandLine.Add('\0', "bb");
-    //        commandLine.Add('c', "");
+            try {
+                var key = commandLine.Add(longKey);
+                Assert.True(expected);
+            } catch(ArgumentException ex) {
+                Assert.False(expected, ex.ToString());
+            }
+        }
 
-    //        try {
-    //            var key = commandLine.Add(shortKey, longKey);
-    //            Assert.True(expected);
-    //        } catch(ArgumentException ex) {
-    //            Assert.False(expected, ex.ToString());
-    //        }
-    //    }
+        [Theory]
+        //[InlineData("A", new[] { "/a", "A" }, "aaa")]
+        [InlineData("A", new[] { "/aaa", "A" }, "aaa")]
+        [InlineData("AA", new[] { "/aaa", "AA", "/a", "A" }, "aaa")]
+        [InlineData("A", new[] { "--aaa", "A" }, "aaa")]
+        [InlineData("AA", new[] { "--aaa", "AA", "-a", "A" },"aaa")]
+        //[InlineData("A", new[] { "/a=A" }, "aaa")]
+        [InlineData("A", new[] { "/aaa=A" }, "aaa")]
+        [InlineData("AA", new[] { "/aaa=AA", "/a=A" }, "aaa")]
+        [InlineData("A", new[] { "--aaa=A" }, "aaa")]
+        [InlineData("AA", new[] { "--aaa=AA", "-a=A" }, "aaa")]
+        [InlineData("A", new[] { "/aaa=\"A\"" }, "aaa")]
+        [InlineData("AA", new[] { "/aaa=\"AA\"", "/a=\"A\"" }, "aaa")]
+        [InlineData("A", new[] { "--aaa=\"A\"" }, "aaa")]
+        [InlineData("AA", new[] { "--aaa=\"AA\"", "-a=\"A\"" }, "aaa")]
+        public void ExecuteTest_Simple(string expected, string[] args, string longKey)
+        {
+            var commandLine = new CommandLine(args, false);
+            var commandKey = commandLine.Add(longKey, true);
 
-    //    [Theory]
-    //    [InlineData("A", new[] { "/a", "A" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "/aaa", "A" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "/aaa", "AA", "/a", "A" }, 'a', "aaa")]
-    //    //[InlineData("A", new[] { "-a", "A" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "--aaa", "A" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "--aaa", "AA", "-a", "A" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "/a=A" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "/aaa=A" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "/aaa=AA", "/a=A" }, 'a', "aaa")]
-    //    //[InlineData("A", new[] { "-a=A" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "--aaa=A" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "--aaa=AA", "-a=A" }, 'a', "aaa")]
-    //    //[InlineData("A", new[] { "/a=\"A\"" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "/aaa=\"A\"" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "/aaa=\"AA\"", "/a=\"A\"" }, 'a', "aaa")]
-    //    //[InlineData("A", new[] { "-a=\"A\"" }, 'a', "aaa")]
-    //    [InlineData("A", new[] { "--aaa=\"A\"" }, 'a', "aaa")]
-    //    [InlineData("AA", new[] { "--aaa=\"AA\"", "-a=\"A\"" }, 'a', "aaa")]
-    //    public void ExecuteTest_Simple(string expected, string[] args, char shortKey, string longKey)
-    //    {
-    //        var commandLine = new CommandLine(args, false);
-    //        var commandKey = commandLine.Add(shortKey, longKey, true);
+            Assert.True(commandLine.Parse());
+            var value = commandLine.Values[commandKey];
+            Assert.True(value.First == expected);
+        }
 
-    //        Assert.True(commandLine.Parse());
-    //        var value = commandLine.Values[commandKey];
-    //        Assert.True(value.First == expected);
-    //    }
+        [Theory]
+        [InlineData(false, new[] { "/a" }, "aaa")]
+        [InlineData(true, new[] { "/aaa" }, "aaa")]
+        //[InlineData(true, new[] { "-a" }, 'a', "aaa")]
+        [InlineData(true, new[] { "--aaa" }, "aaa")]
+        [InlineData(false, new[] { "--aaa" }, "AAA")]
+        public void ExecuteTest_Switch(bool expected, string[] args, string longKey)
+        {
+            var commandLine = new CommandLine(args, false);
+            var commandKey = commandLine.Add(longKey, false);
 
-    //    [Theory]
-    //    [InlineData(true, new[] { "/a" }, 'a', "aaa")]
-    //    [InlineData(true, new[] { "/aaa" }, 'a', "aaa")]
-    //    //[InlineData(true, new[] { "-a" }, 'a', "aaa")]
-    //    [InlineData(true, new[] { "--aaa" }, 'a', "aaa")]
-    //    public void ExecuteTest_Switch(bool expected, string[] args, char shortKey, string longKey)
-    //    {
-    //        var commandLine = new CommandLine(args, false);
-    //        var commandKey = commandLine.Add(shortKey, longKey, false);
+            Assert.True(commandLine.Parse());
+            var has = commandLine.Switches.Contains(commandKey);
+            Assert.True(has == expected);
+        }
 
-    //        Assert.True(commandLine.Parse());
-    //        var has = commandLine.Switches.Contains(commandKey);
-    //        Assert.True(has == expected);
-    //    }
-
-    //    [Theory]
-    //    [InlineData("", "")]
-    //    [InlineData("a", "a")]
-    //    [InlineData("a", " a")]
-    //    [InlineData("a", "a ")]
-    //    [InlineData("a", " a ")]
-    //    [InlineData("\"a a\"", "a a")]
-    //    [InlineData("a\"\"b", "a\"b")]
-    //    [InlineData("a\"\"\"\"\"\"b", "a\"\"\"b")]
-    //    [InlineData("\"a \"\"\"\"\"\" b\"", "a \"\"\" b")]
-    //    public void Escape(string expected, string input)
-    //    {
-    //        var actual = CommandLine.Escape(input);
-    //        Assert.Equal(expected, actual);
-    //    }
-    //}
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("a", " a")]
+        [InlineData("a", "a ")]
+        [InlineData("a", " a ")]
+        [InlineData("\"a a\"", "a a")]
+        [InlineData("a\"\"b", "a\"b")]
+        [InlineData("a\"\"\"\"\"\"b", "a\"\"\"b")]
+        [InlineData("\"a \"\"\"\"\"\" b\"", "a \"\"\" b")]
+        public void Escape(string expected, string input)
+        {
+            var actual = CommandLine.Escape(input);
+            Assert.Equal(expected, actual);
+        }
+    }
 }
