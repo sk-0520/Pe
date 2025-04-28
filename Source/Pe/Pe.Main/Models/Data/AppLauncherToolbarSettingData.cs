7323995badba8e78ce83e83d5136f2c6c9665215
sk <content.type.text.net@gmail.com>
2025-04-28 21:11:59 +0900

    これ required init が正なのでは

--- Source/Pe/Pe.Main/Models/Data/AppLauncherToolbarSettingData.cs
+++ Source/Pe/Pe.Main/Models/Data/AppLauncherToolbarSettingData.cs
@@ -14,15 +14,19 @@ namespace ContentTypeTextNet.Pe.Main.Models.Data
         /// <summary>
         /// ボタンへのD&amp;D実行時の処理方法。
         /// </summary>
-        public LauncherToolbarContentDropMode ContentDropMode { get; set; }
+        public LauncherToolbarContentDropMode ContentDropMode { get; init; }
         /// <summary>
         /// D&amp;Dファイルがショートカットの場合の処理方法。
         /// </summary>
-        public LauncherToolbarShortcutDropMode ShortcutDropMode { get; set; }
+        public LauncherToolbarShortcutDropMode ShortcutDropMode { get; init; }
         /// <summary>
         /// グループメニューの位置。
         /// </summary>
-        public LauncherGroupPosition GroupMenuPosition { get; set; }
+        public LauncherGroupPosition GroupMenuPosition { get; init; }
+        /// <summary>
+        /// アイテムの重複確認方法。
+        /// </summary>
+        public LauncherToolbarDuplicatedFileRegisterMode DuplicatedFileRegisterMode { get; init; }
 
         #endregion
     }
