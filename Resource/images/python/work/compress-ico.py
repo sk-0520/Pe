07dbff674cb38bab2f7623c42e44cfd1cf0b1df5
sk <content.type.text.net@gmail.com>
2025-05-11 19:19:05 +0900

    ぱいそん、わからん
    仕事でもずっと言ってる

---
+++ Resource/images/python/work/compress-ico.py
@@ -0,0 +1,26 @@
+import argparse
+from PIL import Image
+
+def convert(input_path: str, output_path: str) -> None:
+    print(f"{input_path=}, {output_path=}")
+    source_icon = Image.open(input_path).convert('RGBA')
+
+    output_image = Image.new()
+
+    print(f"{source_icon=}")
+    image_sizes = source_icon.info['sizes']
+    #source_icon.
+    for image_size in image_sizes:
+        print(f"{image=}")
+        print(f"{image[0]}")
+
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input')
+    parser.add_argument('--output')
+    args = parser.parse_args()
+
+    convert(args.input, args.output)
