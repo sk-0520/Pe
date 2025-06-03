723741c9e72d52bed77c90b00267d450bc681167
sk <content.type.text.net@gmail.com>
2025-06-03 19:15:14 +0900

    テストしてないけど大丈夫でしょ

--- Source/Pe/Pe.Mvvm/Bindings/NotifyPropertyBase.cs
+++ Source/Pe/Pe.Mvvm/Bindings/NotifyPropertyBase.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Library.Common;
 
 namespace ContentTypeTextNet.Pe.Mvvm.Bindings
 {
@@ -38,4 +39,39 @@ namespace ContentTypeTextNet.Pe.Mvvm.Bindings
         #endregion
     }
 
+    /// <summary>
+    /// 通知モデル基底。
+    /// </summary>
+    public abstract class WeakNotifyPropertyBase: INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler? PropertyChanged
+        {
+            add => PropertyChangedWeakEvent.Add(value);
+            remove => PropertyChangedWeakEvent.Remove(value);
+        }
+
+        #endregion
+
+        #region property
+
+        private PropertyChangedWeakEvent PropertyChangedWeakEvent { get; } = new PropertyChangedWeakEvent(nameof(PropertyChanged));
+
+        #endregion
+
+        #region function
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedWeakEvent.Raise(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged(string notifyPropertyName)
+        {
+            OnPropertyChanged(notifyPropertyName);
+        }
+
+        #endregion
+    }
 }
