3d1a1b6444ca2d25075bbc90fb65d7badeca7c4f
sk <content.type.text.net@gmail.com>
2025-09-27 20:36:41 +0900

    !!!

--- Build/pack-icon.ps1
+++ Build/pack-icon.ps1
@@ -1,5 +1,6 @@
 Param(
 	[Parameter(mandatory = $true)][string] $ArtifactDirectoryPath,
+	[Parameter(mandatory = $true)][string] $OutputDirectoryPath
 )
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
@@ -11,13 +12,10 @@ $types = @(
 	'plugin'
 )
 
-$packedDir = "packed"
-if (!(Test-Path $packedDir)) {
-    New-Item -ItemType Directory -Path $packedDir | Out-Null
+if (!(Test-Path $OutputDirectoryPath)) {
+	New-Item -ItemType Directory -Path $OutputDirectoryPath | Out-Null
 }
 foreach ($type in $types) {
-	$src = Get-ChildItem -Path $ArtifactDirectoryPath -Recurse -Filter App.ico | Where-Object { $_.FullName -match "@data\\$type\\App.ico$" } | Select-Object -First 1
-	if ($src) {
-		Copy-Item $src.FullName (Join-Path $packedDir "App-$type.ico")
-	}
+	$iconPath = Join-Path -Path $ArtifactDirectoryPath -ChildPath "@data" | Join-Path -ChildPath $type | Join-Path -ChildPath "App.ico"
+	Copy-Item -Path $iconPath -Destination (Join-Path -Path $OutputDirectoryPath -ChildPath "App-${type}.ico")
 }
