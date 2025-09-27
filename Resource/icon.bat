0690cc981e88c44293c9d9a95f01ee74656cda9e
sk <content.type.text.net@gmail.com>
2025-09-27 19:34:56 +0900

    必要リソース整理

--- Resource/icon.bat
+++ Resource/icon.bat
@@ -1,6 +1,8 @@
 cd /d %~dp0
 
-powershell -ExecutionPolic Unrestricted -File icon.ps1 -BatchMode -FirstInput 1
-powershell -ExecutionPolic Unrestricted -File icon.ps1 -BatchMode -FirstInput 2
+for %%f in (release debug beta plugin) do (
+	docker compose run retouch pwsh work/EntryRetouch.ps1 -mode %%f
+	docker compose run inkscape /bin/bash -ue /work/export-png.sh --svg /data/@data/%%f/App.svg --output /data/@data/%%f
+	docker compose run --entrypoint "python /work/compress-ico.py --dir /data/@data/%%f --output /data/@data/%%f/App.ico" python
+)
 
-pause
