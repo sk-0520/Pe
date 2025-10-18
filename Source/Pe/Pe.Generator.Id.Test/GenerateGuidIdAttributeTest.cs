fba8af5b489bc947703dc57f1f59b8dd95d77f59
sk <content.type.text.net@gmail.com>
2025-10-18 20:31:17 +0900

    Gen (#1020)
    * ソースジェネレータ、始めます
    
    冷やし中華ももう終わりの時季
    
    * おうおう
    
    * 多分 T4 で作った構造体は網羅できた気がする
    
    * きょーゆーぷろじぇくと
    
    * ネストした型はエラーとした
    
    * テスト用の空プロジェクト追加
    
    * 構造整理
    
    * windows update 襲来
    
    * 移し替え！
    
    * T4 を無理になくす必要はないのでは、なかろうか、手間が。。。
    
    * さすがにこれは手で書いた方が早い
    
    * 関係ある？
    
    * AnyCPU
    
    * 不要分破棄
    
    * あああああ！ AI君がなんの解決にもならない！！
    
    * PR での修正
    
    * これいんの？

---
+++ Source/Pe/Pe.Generator.Id.Test/GenerateGuidIdAttributeTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Generator.Id;
+using Xunit;
+
+namespace ContentTypeTextNet.Pe.Generator.Id.Test
+{
+    /// <summary>
+    /// テストデータ。
+    /// </summary>
+    [GenerateGuidId()]
+    public partial record struct TestData;
+
+    //[GenerateGuidIdTypeHandler(typeof(TestData))]
+    //public partial class TestDataHandler;
+
+    public class GenerateGuidIdAttributeTest
+    {
+        #region function
+
+        [Fact]
+        public void Constructor_Empty_Test()
+        {
+            var actual = new TestData();
+            Assert.Equal(TestData.Empty, actual);
+        }
+
+        #endregion
+    }
+}
