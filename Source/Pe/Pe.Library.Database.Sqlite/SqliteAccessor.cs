df6983139aa2cc72ea3b836c1c36782ccc097ba6
sk <content.type.text.net@gmail.com>
2025-10-26 22:40:22 +0900

    こういう方針

--- Source/Pe/Pe.Library.Database.Sqlite/SqliteAccessor.cs
+++ Source/Pe/Pe.Library.Database.Sqlite/SqliteAccessor.cs
@@ -1,16 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SQLite;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using ContentTypeTextNet.Pe.Library.Database;
 using Microsoft.Extensions.Logging;
 
-namespace ContentTypeTextNet.Pe.Core.Models.Database.Vender.Public.SQLite
+namespace ContentTypeTextNet.Pe.Library.Database.Sqlite
 {
-    [Obsolete("Pe.Library.Database.Sqlite")]
     public class SqliteAccessor: DatabaseAccessor<SQLiteConnection>
     {
         public SqliteAccessor(IDatabaseFactory databaseFactory, ILogger logger)
