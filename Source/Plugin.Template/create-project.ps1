08ad024a220284fa2afb0583bfd050278e897a58
sk <content.type.text.net@gmail.com>
2025-12-10 20:19:20 +0900

    外部コマンド処理を関数化

--- Source/Plugin.Template/create-project.ps1
+++ Source/Plugin.Template/create-project.ps1
@@ -38,6 +38,7 @@ Pe リポジトリからいい感じのあれこれを取ってきてあれこ�
 https://github.com/sk-0520/Pe
 #>
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+[CmdletBinding(SupportsShouldProcess)]
 param(
 	[Parameter(mandatory = $true)][string] $ProjectDirectory,
 	[Parameter(mandatory = $true)][string] $PluginName,
@@ -116,8 +117,49 @@ if ($reservedNames.Contains($PluginName)) {
 	throw ('予約済みプラグイン名: ' + $PluginName)
 }
 
+#===================================================
+
+function Start-Command {
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param (
+		[Parameter(Mandatory = $true)][string] $Command,
+		[Parameter(Mandatory = $false)][string[]] $ArgumentList
+	)
+
+	if ($PSCmdlet.ShouldProcess($Command, $ArgumentList)) {
+		# $?/$LASTEXITCODE はコマンドに左右されるので呼び出し側で判定する
+		& $Command $ArgumentList
+	} else {
+		Write-Verbose ('[DRY] 実行予定: {0} {1}' -f $Command, $ArgumentList)
+	}
+}
+
+function Start-Git {
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param (
+		[Parameter(Mandatory = $true)][string[]] $ArgumentList
+	)
+	Start-Command -Command $parameters.git -ArgumentList $ArgumentList
+	Write-Verbose "git: $LASTEXITCODE"
+	if ($LASTEXITCODE -ne 0) {
+		throw "git ${ArgumentList}: $LASTEXITCODE"
+	}
+}
+
+function Start-DotNet {
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param (
+		[Parameter(Mandatory = $true)][string[]] $ArgumentList
+	)
+	Start-Command -Command $parameters.dotnet -ArgumentList $ArgumentList
+	Write-Verbose "dotnet: $LASTEXITCODE"
+	if ($LASTEXITCODE -ne 0) {
+		throw "dotnet ${ArgumentList}: $LASTEXITCODE"
+	}
+}
 
 #===================================================
+
 # 各種諸々の生成
 $parameters = @{
 	pluginName = $PluginName
@@ -151,10 +193,12 @@ Write-Verbose "`tDotNetPath: ${DotNetPath}"
 
 Write-Information '情報:'
 $parameters | Format-Table -AutoSize
-Write-Information ('git: ' + (& $parameters.git --version))
-Write-Information ('dotnet: ' + (& $parameters.dotnet --version))
+Write-Information ('git: ' + (Start-Git -ArgumentList --version))
+Write-Information ('dotnet: ' + (Start-DotNet -ArgumentList --version))
 
 #---------------------------------------------------
+
+
 function Convert-TemplateValue {
 	[OutputType([string])]
 	param(
@@ -253,10 +297,7 @@ if ((Get-ChildItem -Path $parameters.directory -Recurse -Force | Measure-Object)
 
 Write-Information "プロジェクトディレクトリ生成: $($parameters.directory)"
 if (!$suppressScm) {
-	& $parameters.git init $parameters.directory
-	if (!$?) {
-		throw "git init: $LASTEXITCODE"
-	}
+	Start-Git -ArgumentList init, $parameters.directory
 }
 
 Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Template\*') -Destination ($parameters.directory.FullName + '\') -Force -Recurse
@@ -278,22 +319,13 @@ function New-Submodule {
 		$targetPath = Join-Path -Path $parameters.directory -ChildPath $Path
 		Write-Information "サブモジュール親ディレクトリ生成: $targetPath"
 		if ($PSCmdlet.ShouldProcess('Path', "$Path のテンプレート文字列を置き換え")) {
-			& $parameters.git submodule add --branch $Branch $Uri $Path
-			if (!$?) {
-				throw "git submodule: $LASTEXITCODE"
-			}
+			Start-Git -ArgumentList submodule, add, --branch, $Branch, $Uri, $Path
 
 			if (${Revision}) {
 				Push-Location -LiteralPath $Path
-				& $parameters.git checkout "${Revision}"
-				if (!$?) {
-					throw "git checkout: $LASTEXITCODE"
-				}
+				Start-Git -ArgumentList checkout, "${Revision}"
 				Pop-Location
-				& $parameters.git add .
-				if (!$?) {
-					throw "git add: $LASTEXITCODE"
-				}
+				Start-Git -ArgumentList add, .
 			}
 		} else {
 			Write-Verbose "`[DRY`] $($parameters.git) submodule add --branch $Branch $Uri $Path"
@@ -319,10 +351,7 @@ try {
 
 	foreach ($pluginTarget in $pluginTargets) {
 		Write-Verbose "プロジェクトを追加: $pluginTarget"
-		& $parameters.dotnet sln add $pluginTarget
-		if (!$?) {
-			throw "dotnet sln add ${pluginTarget}: $LASTEXITCODE"
-		}
+		Start-DotNet -ArgumentList sln, add, $pluginTarget
 	}
 
 	Write-Verbose 'Peを追加'
@@ -387,10 +416,7 @@ try {
 	)
 	foreach ($item in $items) {
 		$projectFilePath = Join-Path -Path $appDir -ChildPath $item.project | Join-Path -ChildPath ($item.project + '.csproj')
-		& $parameters.dotnet sln add $projectFilePath --solution-folder $item.directory
-		if (!$?) {
-			throw "dotnet sln add ${projectFilePath}: $LASTEXITCODE"
-		}
+		Start-DotNet -ArgumentList sln, add, $projectFilePath, --solution-folder, $item.directory
 	}
 
 	$solutionFileName = "${PluginName}.slnx"
@@ -406,10 +432,7 @@ try {
 
 	Write-Verbose 'NuGet 復元'
 	if (!$suppressBuild) {
-		& $parameters.dotnet restore
-		if (!$?) {
-			throw "dotnet restore: $LASTEXITCODE"
-		}
+		Start-DotNet -ArgumentList restore
 	}
 
 	Write-Verbose 'プラグイン起動設定追加'
@@ -428,22 +451,13 @@ try {
 
 	if (!$suppressBuild) {
 		Write-Verbose 'とりあえずのデバッグ全ビルド'
-		& $parameters.dotnet build --configuration Debug /p:Platform=x64
-		if (!$?) {
-			throw "dotnet build: $LASTEXITCODE"
-		}
+		Start-DotNet -ArgumentList build, --configuration, Debug, /p:Platform=x64
 	}
 
 	if (!$suppressScm) {
 		Write-Verbose 'はいコミット'
-		& $parameters.git add --all
-		if (!$?) {
-			throw "git add: $LASTEXITCODE"
-		}
-		& $parameters.git commit --message "initialize $PluginName"
-		if (!$?) {
-			throw "git commit: $LASTEXITCODE"
-		}
+		Start-Git -ArgumentList add, --all
+		Start-Git -ArgumentList commit, --message, "initialize $PluginName"
 	}
 } finally {
 	Pop-Location
