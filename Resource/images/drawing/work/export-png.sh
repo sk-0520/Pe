8b76bb50b90dac23d4a77b4cc78551c65794a4bc
sk <content.type.text.net@gmail.com>
2025-12-14 20:15:42 +0900

    shelldre:S7688

--- Resource/images/drawing/work/export-png.sh
+++ Resource/images/drawing/work/export-png.sh
@@ -2,7 +2,7 @@
 
 ARGS=$(getopt -o '' -l "svg:,output:" -- "$@") || exit 1
 eval "set -- $ARGS"
-while [ $# -gt 0 ]; do
+while [[ $# -gt 0 ]]; do
 	case $1 in
 		--svg) SOURCE_SVG_PATH=$2; shift 2 ;;
 		--output) OUTPUT_DIR_PATH=$2; shift 2 ;;
@@ -29,7 +29,7 @@ for ICON_SIZE in "${ICON_SIZE_LIST[@]}"; do
 		--export-filename="${OUTPUT_DIR_PATH}/${ICON_SIZE}.png" \
 		"${SOURCE_SVG_PATH}"
 
-	if [ "${ICON_SIZE}" -ge 60 ]; then
+	if [[ "${ICON_SIZE}" -ge 60 ]]; then
 		echo "Optimizing ${OUTPUT_DIR_PATH}/${ICON_SIZE}.png"
 		echo -n "SRC: "
 		ls -l "${OUTPUT_DIR_PATH}/${ICON_SIZE}.png"
