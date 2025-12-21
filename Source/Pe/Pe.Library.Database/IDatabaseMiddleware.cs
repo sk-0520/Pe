cda7c13d7a313f3567051b40209842fe623d73f0
sk <content.type.text.net@gmail.com>
2025-12-21 20:23:00 +0900

    なんも考えまとまってない

---
+++ Source/Pe/Pe.Library.Database/IDatabaseMiddleware.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentTypeTextNet.Pe.Library.Database
+{
+    public interface IDatabaseStatementMiddleware
+    {
+        #region function
+
+
+        #endregion
+    }
+
+    public interface IDatabaseQueryMiddleware
+    {
+        #region function
+
+
+        #endregion
+    }
+
+    public interface IDatabaseExecuteMiddleware
+    {
+        #region function
+
+
+        #endregion
+    }
+
+}
