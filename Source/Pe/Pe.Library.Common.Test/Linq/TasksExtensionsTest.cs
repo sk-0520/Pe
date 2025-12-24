8046e625d2455afce0f72c0381c419b570de9c71
sk <content.type.text.net@gmail.com>
2025-12-24 22:01:37 +0900

    TasksExtensionsTest

---
+++ Source/Pe/Pe.Library.Common.Test/Linq/TasksExtensionsTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContentTypeTextNet.Pe.Library.Common.Test.Linq
+{
+    public class TasksExtensionsTest
+    {
+        #region function
+
+        [Fact]
+        public void Task_ThrowIfHasException_None_Test()
+        {
+            var task = Task.CompletedTask;
+            var exception = Record.Exception(() => task.ThrowIfHasException());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void Task_ThrowIfHasException_Exception_Test()
+        {
+            var task = Task.FromException(new InvalidOperationException());
+            Assert.Throws<InvalidOperationException>(() => task.ThrowIfHasException());
+        }
+
+        [Fact]
+        public void ValueTask_ThrowIfHasException_None_Test()
+        {
+            var task = ValueTask.CompletedTask;
+            var exception = Record.Exception(() => task.ThrowIfHasException());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ValueTask_ThrowIfHasException_Exception_Test()
+        {
+            var task = ValueTask.FromException(new InvalidOperationException());
+            Assert.Throws<InvalidOperationException>(() => task.ThrowIfHasException());
+        }
+
+        #endregion
+    }
+}
