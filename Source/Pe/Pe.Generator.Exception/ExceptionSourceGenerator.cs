1169181b896b22339e1685b0044c03b1ce5ac5cb
sk <content.type.text.net@gmail.com>
2026-01-01 13:08:40 +0900

    正月に何やってんだ

--- Source/Pe/Pe.Generator.Exception/ExceptionSourceGenerator.cs
+++ Source/Pe/Pe.Generator.Exception/ExceptionSourceGenerator.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 
 namespace ContentTypeTextNet.Pe.Generator.Exception
@@ -9,6 +9,146 @@ namespace ContentTypeTextNet.Pe.Generator.Exception
     [Generator(LanguageNames.CSharp)]
     public class ExceptionSourceGenerator: IIncrementalGenerator
     {
+        #region function
+
+        private static string ToAttributeCode(SourceBuilder sourceBuilder, AttributeData attr)
+        {
+            var className = attr.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ?? "";
+            var args = new List<string>();
+
+            foreach(var c in attr.ConstructorArguments) {
+                args.Add(sourceBuilder.ToCode(c));
+            }
+            foreach(var na in attr.NamedArguments) {
+                args.Add(na.Key + " = " + sourceBuilder.ToCode(na.Value));
+            }
+
+            var argText = args.Count > 0 ? "(" + string.Join(", ", args) + ")" : "";
+            return "[" + className + argText + "]";
+        }
+
+        private static string ToConstructorParameterCode(IParameterSymbol parameter)
+        {
+            var t = parameter.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+            var name = parameter.Name;
+            return t + " " + name;
+        }
+
+        private static string GenerateSourceCore(SourceBuilder sourceBuilder, INamedTypeSymbol baseType, INamedTypeSymbol targetSymbol)
+        {
+            var targetName = targetSymbol.Name;
+
+            // クラス属性を親から持ってくる＋謎の SerializableAttribute を追加
+            var classAttributeCodes = sourceBuilder
+                .FilterAttribute(baseType.GetAttributes())
+                .Select(a => ToAttributeCode(sourceBuilder, a))
+                .Prepend("[global::System.SerializableAttribute]")
+                .Distinct()
+                .ToArray()
+            ;
+
+            // 親の非 private コンストラクタを列挙
+            var constructors = baseType.InstanceConstructors
+                .Where(c => c.DeclaredAccessibility != Accessibility.Private)
+                .ToArray()
+            ;
+
+            var constructorCodes = new List<string>();
+
+            foreach(var ctor in constructors) {
+                // コンストラクタ属性をコピー
+                var constructorAttributeCodes = sourceBuilder
+                    .FilterAttribute(ctor.GetAttributes())
+                    .Select(a => ToAttributeCode(sourceBuilder, a))
+                    .ToArray()
+                ;
+
+                var accessibility = sourceBuilder.ToCode(ctor.DeclaredAccessibility);
+
+                var parameters = ctor.Parameters.Select(a => ToConstructorParameterCode(a));
+                var args = ctor.Parameters.Select(p => p.Name);
+
+                var signature = $"{accessibility} {targetName}({string.Join(", ", parameters)})";
+
+                var constructorCode = $$"""
+
+                    {{string.Join("", constructorAttributeCodes)}}
+                    {{signature}}
+                        : base({{string.Join(", ", args)}})
+                    { }
+
+                """;
+                constructorCodes.Add(constructorCode);
+            }
+
+            var classAccessibilityCode = sourceBuilder.ToCode(targetSymbol.DeclaredAccessibility);
+
+            var source = $$"""
+{{sourceBuilder.Header}}
+
+{{(sourceBuilder.ToNamespaceCode(targetSymbol))}}
+
+{{string.Join("", classAttributeCodes)}}
+{{classAccessibilityCode}} partial class {{targetName}}
+{
+{{string.Join("", constructorCodes)}}
+}
+
+""";
+            return source;
+        }
+
+        private static void GenerateSource(SourceProductionContext context, ImmutableArray<GeneratorAttributeSyntaxContext> array)
+        {
+            var sourceBuilder = new SourceBuilder();
+
+            // マークされた型を集める（ジェネレータ対象の型集合）
+            var marked = new HashSet<INamedTypeSymbol>(SymbolEqualityComparer.Default);
+            foreach(var attrCtx in array) {
+                if(attrCtx.TargetSymbol is INamedTypeSymbol nts) {
+                    marked.Add(nts);
+                }
+            }
+
+            foreach(var attribute in array) {
+                var targetSymbol = (INamedTypeSymbol)attribute.TargetSymbol;
+
+                // ネストは無理
+                if(targetSymbol.ContainingType != null) {
+                    var location = targetSymbol.Locations.Length > 0 ? targetSymbol.Locations[0] : Location.None;
+                    context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NestedTypeNotSupported, location, targetSymbol.ToDisplayString()));
+                    continue;
+                }
+
+                var baseType = targetSymbol.BaseType;
+                if(baseType is null) {
+                    continue;
+                }
+
+                // ベースがジェネレータで生成される型の場合、その型の親へ遡る
+                var effectiveBase = baseType;
+                while(effectiveBase != null) {
+                    // 同一コンパイル内でマーク済みかを判定
+                    if(marked.Contains(effectiveBase)) {
+                        effectiveBase = effectiveBase.BaseType;
+                        continue;
+                    }
+                    break;
+                }
+
+                if(effectiveBase is null) {
+                    effectiveBase = baseType;
+                }
+
+                var source = GenerateSourceCore(sourceBuilder, effectiveBase, targetSymbol);
+                var sourceFileName = sourceBuilder.ToSourceFilePath(targetSymbol);
+                context.AddSource(sourceFileName, source);
+            }
+        }
+
+        #endregion
+
+
         #region IIncrementalGenerator
 
         public void Initialize(IncrementalGeneratorInitializationContext context)
@@ -46,44 +186,6 @@ namespace {{attributeNamespace}}
             ).Collect();
 
             context.RegisterSourceOutput(provider, GenerateSource);
-        }
-
-        private void GenerateSource(SourceProductionContext context, ImmutableArray<GeneratorAttributeSyntaxContext> array)
-        {
-            var sourceBuilder = new SourceBuilder();
-
-            foreach(var attribute in array) {
-                var targetSymbol = (INamedTypeSymbol)attribute.TargetSymbol;
-
-                // ネストは無理
-                if(targetSymbol.ContainingType != null) {
-                    var location = targetSymbol.Locations.Length > 0 ? targetSymbol.Locations[0] : Location.None;
-                    context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NestedTypeNotSupported, location, targetSymbol.ToDisplayString()));
-                    continue;
-                }
-
-                var namespaceName = targetSymbol.ContainingNamespace.IsGlobalNamespace
-                    ? ""
-                    : targetSymbol.ContainingNamespace.ToString();
-
-                var targetName = targetSymbol.Name;
-                var fullName = (targetSymbol.ContainingNamespace.IsGlobalNamespace ? "" : namespaceName + ".") + targetName;
-
-                var source = $$"""
-{{sourceBuilder.Header}}
-using System;
-using System.Text.Json.Serialization;
-
-{{(sourceBuilder.ToNamespaceCode(targetSymbol))}}
-
-partial class {{targetName}}
-{
-}
-
-""";
-                var sourceFileName = sourceBuilder.ToSourceFilePath(targetSymbol);
-                context.AddSource(sourceFileName, source);
-            }
         }
 
         #endregion
--- Source/Pe/Pe.Generator.Test/SourceBuilderTest.cs
+++ Source/Pe/Pe.Generator.Test/SourceBuilderTest.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Xunit;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Bug", "S3903:Types should be defined in named namespaces", Justification = "<保留中>")]
@@ -80,6 +77,29 @@ namespace ContentTypeTextNet.Pe.Generator.Test
             var actual = sourceBuilder.ToCode<InnerClass>();
             var expected = "global::ContentTypeTextNet.Pe.Generator.Test.SourceBuilderTest+InnerClass";
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("private", Accessibility.Private)]
+        [InlineData("private protected", Accessibility.ProtectedAndInternal)]
+        [InlineData("protected", Accessibility.Protected)]
+        [InlineData("internal", Accessibility.Internal)]
+        [InlineData("protected internal", Accessibility.ProtectedOrInternal)]
+        [InlineData("public", Accessibility.Public)]
+        public void ToCode_Accessibility_Test(string expected, Accessibility accessibility)
+        {
+            var sourceBuilder = new SourceBuilder();
+            var actual = sourceBuilder.ToCode(accessibility);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(Accessibility.NotApplicable)]
+        [InlineData(Accessibility.Friend)]
+        public void ToCode_Accessibility_Throw_Test(Accessibility accessibility)
+        {
+            var sourceBuilder = new SourceBuilder();
+            Assert.Throws<NotSupportedException>(() => sourceBuilder.ToCode(accessibility));
         }
 
         #endregion
