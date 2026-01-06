4bbe5e5bc05fb153ec475a7c422a8756c81e4896
sk <content.type.text.net@gmail.com>
2026-01-06 22:06:29 +0900

    カバレッジだけ対応する💩みたいな処理

--- Source/Pe/Pe.Library.Args/CommandLineParserExtensions.cs
+++ Source/Pe/Pe.Library.Args/CommandLineParserExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Text;
 
@@ -16,49 +17,53 @@ namespace ContentTypeTextNet.Pe.Library.Args
         /// <inheritdoc cref="CommandLineParser.Add(CommandLineOption)"/>
         public static CommandLineOption Add(this CommandLineParser parser, string key, CommandLineOptionKind kind) => Add(parser, key, kind, string.Empty);
 
+        private static string ToUsageCore(CommandLineHelper helper, CommandLineOption option)
+        {
+            var builder = new StringBuilder();
+
+            var notRequired = !option.Required || option.Kind == CommandLineOptionKind.Switch;
+
+            if(notRequired) {
+                builder.Append('[');
+            }
+
+            builder.Append(helper.OptionPrefix);
+            builder.Append(option.Key);
+
+            if(option.Kind == CommandLineOptionKind.Value) {
+                builder.Append(helper.Separator);
+                builder.Append('<');
+                if(string.IsNullOrWhiteSpace(option.ValueName)) {
+                    builder.Append("value");
+                } else {
+                    builder.Append(option.ValueName);
+                }
+                builder.Append('>');
+            }
+
+            if(notRequired) {
+                builder.Append(']');
+            }
+
+            if(!string.IsNullOrWhiteSpace(option.Description)) {
+                builder.AppendLine();
+                builder.Append(helper.DescriptionIndent);
+                builder.Append(option.Description);
+            }
+
+            return builder.ToString();
+        }
+
         public static string ToUsage(this CommandLineParser parser)
         {
             var items = parser.Options
                 .OrderBy(a => a.Value.Required ? 0 : 1)
                 .ThenBy(a => a.Value.Kind == CommandLineOptionKind.Value ? 0 : 1)
                 .ThenBy(a => a.Key)
-                .Select(a => {
-                    var builder = new StringBuilder();
-
-                    var notRequired = !a.Value.Required || a.Value.Kind == CommandLineOptionKind.Switch;
-
-                    if(notRequired) {
-                        builder.Append('[');
-                    }
-
-                    builder.Append(parser.Helper.OptionPrefix);
-                    builder.Append(a.Key);
-
-                    if(a.Value.Kind == CommandLineOptionKind.Value) {
-                        builder.Append(parser.Helper.Separator);
-                        builder.Append('<');
-                        if(string.IsNullOrWhiteSpace(a.Value.ValueName)) {
-                            builder.Append("value");
-                        } else {
-                            builder.Append(a.Value.ValueName);
-                        }
-                        builder.Append('>');
-                    }
-
-                    if(notRequired) {
-                        builder.Append(']');
-                    }
-
-                    if(!string.IsNullOrWhiteSpace(a.Value.Description)) {
-                        builder.AppendLine();
-                        builder.Append(parser.Helper.DescriptionIndent);
-                        builder.Append(a.Value.Description);
-                    }
-
-                    return builder.ToString();
-                })
+                .Select(a => ToUsageCore(parser.Helper, a.Value))
             ;
-            return string.Join(System.Environment.NewLine, items);
+
+            return string.Join(Environment.NewLine, items);
         }
 
         #endregion
