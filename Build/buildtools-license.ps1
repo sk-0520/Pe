7ce6fa10fae7d89613b20c0ce403f57910b2336d
sk <content.type.text.net@gmail.com>
2026-01-08 21:21:45 +0900

    さぁどうだ

---
+++ Build/buildtools-license.ps1
@@ -0,0 +1,23 @@
+﻿Param(
+	[Parameter(mandatory = $true)][string] $BuildToolsLicense
+)
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Import-Module "${PSScriptRoot}/Modules/Project"
+Import-Module "${PSScriptRoot}/Modules/Command"
+
+
+#/*[FUNCTIONS]-------------------------------------
+#*/[FUNCTIONS]-------------------------------------
+
+$sourceDirectory = Get-SourceDirectory -Kind 'main'
+$centralPackagePath = Join-Path -Path (Get-RootDirectory) -ChildPath 'Directory.Packages.props'
+$componentFilePath = Join-Path -Path $sourceDirectory -ChildPath 'Pe.Main' | Join-Path -ChildPath 'doc' | Join-Path -ChildPath  'component.json'
+$outputFilePath = Join-Path -Path (Get-RootDirectory) -ChildPath 'component.json'
+
+Write-Information 'License'
+if (Test-Path $OutputFile) {
+	Remove-Item -Path $OutputFile
+}
+Start-Command -Command "$BuildToolsLicense" -ArgumentList @("--central-package", $centralPackagePath, "base", $componentFilePath, "--output", $outputFilePath)
