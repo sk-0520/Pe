e323249ca846e35d2bf84a840ec07744b53d3260
sk <content.type.text.net@gmail.com>
2026-01-17 18:12:03 +0900

    add memo

--- Source/Pe/Pe.Mvvm/Commands/CommandFactory.cs
+++ Source/Pe/Pe.Mvvm/Commands/CommandFactory.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ContentTypeTextNet.Pe.Mvvm.Commands
 {
+    // TODO: 生成処理はこいつに寄せておきたい
     public class CommandFactory: ICommandFactory
     {
         #region function
